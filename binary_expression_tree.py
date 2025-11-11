cec0d34a32036d815284450e54212d3632015487
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-11-11 13:52:55 -0500

    Created a skeleton for the Binary Expression Tree class

--- binary_expression_tree.py
+++ binary_expression_tree.py
@@ -4,6 +4,42 @@
 # 2.) EVALUATE tree by using a recursive algorithm
 
 from stack import Stack
+from tree_node import TreeNode
 
 class BinaryExpressionTree():
-    def __init__(self):
+    def __init__(self, root): # constructor that creates an empty tree
+        self.root = None
+
+    ## MAIN FUNCTIONS ##
+
+    def is_empty(self): # booleen that checks if the tree is empty
+        if self.root == None:
+            return True
+        else:
+            return False
+
+    def clear_Tree(self): # clears the tree
+        self.root == None
+
+    def build_from_postfix(self): # builds expression tree representing the postfix expression
+        pass
+
+    def evaluate_tree(self): # evaluates the expression tree and returns the result
+        pass
+
+    def infix_traversal(self): # returns an infix string w/ parentheses for clarity
+        pass
+
+    def postfix_traversal(self): # returns a space-seperated postfix string
+        pass
+
+    ## HELPER FUNCTIONS ##
+    
+    def _inorder(self): # Appends values to out using infix order with parentheses.
+        pass
+
+    def _postorder(self): # Appends values to out in postfix order.
+        pass
+
+    def _evaluate(self): # Recursively computes value of subtree.
+        pass
